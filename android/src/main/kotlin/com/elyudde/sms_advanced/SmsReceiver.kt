6c21bd68fcc57fb35807151bdcde6dbabff60caf
juandpt03 <juandpt03@gmail.com>
2024-06-29 23:30:41 -0500

    revert: restore SmsReceiver class to original implementation

--- android/src/main/kotlin/com/elyudde/sms_advanced/SmsReceiver.kt
+++ android/src/main/kotlin/com/elyudde/sms_advanced/SmsReceiver.kt
@@ -21,37 +21,66 @@ import io.flutter.plugin.common.PluginRegistry.RequestPermissionsResultListener
 import org.json.JSONObject
 import java.util.*
 
-class SmsReceiver : EventChannel.StreamHandler, RequestPermissionsResultListener {
 
-    private var context: Context? = null
-    private var binding: ActivityPluginBinding? = null
-    private var receiver: SmsBroadcastReceiver? = null
-    private var permissions: Permissions? = null
-    private val permissionsList = arrayOf(Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS)
+/**
+ * Created by crazygenius on 1/08/21.
+ */
+internal class SmsReceiver(val context: Context, private val binding: ActivityPluginBinding) : EventChannel.StreamHandler,
+    RequestPermissionsResultListener {
+    private var receiver: BroadcastReceiver? = null
+    private val permissions: Permissions = Permissions(context, binding.activity as FlutterFragmentActivity)
+    private val permissionsList =
+        arrayOf(Manifest.permission.RECEIVE_SMS, Manifest.permission.READ_SMS)
     private var sink: EventSink? = null
-
-    // Constructor sin argumentos
-    constructor()
-
-    // Constructor con argumentos
-    constructor(context: Context, binding: ActivityPluginBinding) {
-        this.context = context
-        this.binding = binding
-        this.permissions = Permissions(context, binding.activity as FlutterFragmentActivity)
-        binding.addRequestPermissionsResultListener(this)
-    }
 
     @TargetApi(Build.VERSION_CODES.KITKAT)
     override fun onListen(arguments: Any?, events: EventSink) {
-        receiver = SmsBroadcastReceiver(events)
-        context?.registerReceiver(receiver, IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION))
+        receiver = createSmsReceiver(events)
+        context
+            .registerReceiver(receiver, IntentFilter(Telephony.Sms.Intents.SMS_RECEIVED_ACTION))
         sink = events
-        permissions?.checkAndRequestPermission(permissionsList, Permissions.RECV_SMS_ID_REQ)
+        permissions.checkAndRequestPermission(permissionsList, Permissions.RECV_SMS_ID_REQ)
     }
 
-    override fun onCancel(arguments: Any?) {
-        context?.unregisterReceiver(receiver)
+    override fun onCancel(o: Any?) {
+        context.unregisterReceiver(receiver)
         receiver = null
+    }
+
+    @TargetApi(Build.VERSION_CODES.KITKAT)
+    private fun readMessages(intent: Intent): Array<SmsMessage> {
+        return Telephony.Sms.Intents.getMessagesFromIntent(intent)
+    }
+
+    private fun createSmsReceiver(events: EventSink): BroadcastReceiver {
+        return object : BroadcastReceiver() {
+            @TargetApi(Build.VERSION_CODES.KITKAT)
+            override fun onReceive(context: Context, intent: Intent) {
+                try {
+                    val msgs = readMessages(intent) ?: return
+                    val obj = JSONObject()
+                    obj.put("address", msgs[0].originatingAddress)
+                    obj.put("date", Date().time)
+                    obj.put("date_sent", msgs[0].timestampMillis)
+                    obj.put(
+                        "read",
+                        if (msgs[0].statusOnIcc == SmsManager.STATUS_ON_ICC_READ) 1 else 0
+                    )
+                    obj.put(
+                        "thread_id",
+                        TelephonyCompat.getOrCreateThreadId(context, msgs[0].originatingAddress!!)
+                    )
+                    var body = ""
+                    for (msg in msgs) {
+                        body += msg.messageBody
+                    }
+                    obj.put("body", body)
+                    events.success(obj)
+                } catch (e: Exception) {
+                    Log.d("SmsReceiver", e.toString())
+                }
+            }
+        }
     }
 
     override fun onRequestPermissionsResult(
@@ -62,40 +91,21 @@ class SmsReceiver : EventChannel.StreamHandler, RequestPermissionsResultListener
         if (requestCode != Permissions.RECV_SMS_ID_REQ) {
             return false
         }
-        val allGranted = grantResults.all { it == PackageManager.PERMISSION_GRANTED }
-        if (!allGranted) {
-            sink?.endOfStream()
-        }
-        return allGranted
-    }
-
-    // Clase interna para manejar la recepción de mensajes SMS
-    inner class SmsBroadcastReceiver(private val events: EventSink?) : BroadcastReceiver() {
-        @TargetApi(Build.VERSION_CODES.KITKAT)
-        override fun onReceive(context: Context, intent: Intent) {
-            try {
-                val msgs = Telephony.Sms.Intents.getMessagesFromIntent(intent) ?: return
-                val obj = JSONObject()
-                obj.put("address", msgs[0].originatingAddress)
-                obj.put("date", Date().time)
-                obj.put("date_sent", msgs[0].timestampMillis)
-                obj.put(
-                    "read",
-                    if (msgs[0].statusOnIcc == SmsManager.STATUS_ON_ICC_READ) 1 else 0
-                )
-                obj.put(
-                    "thread_id",
-                    TelephonyCompat.getOrCreateThreadId(context, msgs[0].originatingAddress!!)
-                )
-                var body = ""
-                for (msg in msgs) {
-                    body += msg.messageBody
-                }
-                obj.put("body", body)
-                events?.success(obj)
-            } catch (e: Exception) {
-                Log.d("SmsReceiver", e.toString())
+        var isOk = true
+        for (res in grantResults) {
+            if (res != PackageManager.PERMISSION_GRANTED) {
+                isOk = false
+                break
             }
         }
+        if (isOk) {
+            return true
+        }
+        sink!!.endOfStream()
+        return false
+    }
+
+    init {
+        binding.addRequestPermissionsResultListener(this)
     }
 }
